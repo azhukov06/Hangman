42cbdad7dc081e8904d60ba3da0a461f5da0c6ac
Artem Zhukov <artem.zhukov06@gmail.com>
2025-08-10 19:36:50 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -36,7 +36,6 @@ int main(int argc, char *argv[]) {
 			cout << "Please enter a letter: ";
 			cin >> c;
 			if (cin.eof()) {
-				cout << endl;
 				return 0;
 			}
 			if (!cin || !(('a' <= c) && (c <= 'z'))) {
